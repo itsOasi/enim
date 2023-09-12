850716fea2788b93a627279cb38038b729546849
itsOasi <ostally001@gmail.com>
2023-09-12 12:02:58 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from './Header'
+import Container1 from './Container1'
+import Container2 from './Container2'
+import Footer from './Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+		<Header />
+		<Container1 />
+		<Container2 />
+		<Footer />
+    </>
+  )
+}
+
+export default App
