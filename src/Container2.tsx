15685e99dc7e5eb205de2f41811546081d86d545
itsOasi <ostally001@gmail.com>
2023-09-12 12:25:08 -0700

    initial commit

---
+++ src/Container2.tsx
@@ -0,0 +1,15 @@
+import './Container2.css'
+
+function Container2(){
+	return (
+	<div id="container2" className='container'>
+		<div>
+			<h2>Join the Membership</h2>
+			<p className="smaller">Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis.</p>
+		</div>
+		<button className="btn btn-secondary">Sign Up</button>
+	</div>
+	)
+}
+
+export default Container2
